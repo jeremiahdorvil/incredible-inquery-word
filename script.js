018591443fd6d271604efd629bceb9ec52c25690
Rampage Ray <jeremiahdorvil@outlook.com>
2022-12-09 22:36:23 -0500

    added variables and changing repository name.

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// Array of letter choices for computer to pick from
+var letters = ["abcdefghijklmnopqrstuvwxyz"].split("")
+
+// Array of number choices for computer to pick from
+var numbers = ["1234567890"].split("")
+
+// Array of special characters for computer to pick from
+var special_characters = ["~!@#$%^&*()_`-[]{};:,./<>?"].split("")
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -8,7 +17,22 @@ function writePassword() {
 
   passwordText.value = password;
 
+  // When the user presses the generate button on screen, computer will ask a series of questions to figure out how to generate the custom password
+
+    var userChoice = generateBtn.prompt
+    ("How many characters would you like for your custom password?")
+
+    input
+    
+    ("Would you like to use lowercase letters?")
+
+  if(!userChoice) {
+    return; else{
+        letters.toLowerCase()
+    }
+  }
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
